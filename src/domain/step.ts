73a55fa78be701d22f7a6f46305e8aa0d07bbc1c
Vladislav <ivanovlad2010@gmail.com>
2023-11-25 16:40:29 +0100

    step model created

---
+++ src/domain/step.ts
@@ -0,0 +1,30 @@
+export type Step = {
+    completed: boolean;
+    question: string;
+    type: StepType;
+    response?: string;
+    options?: SelectOption[];
+};
+
+export type CompletedStep = Omit<Step, "completed"> & {
+    completed: true;
+    response: NonNullable<Step["response"]>;
+};
+export const isStepCompleted = (s: Step): s is CompletedStep =>
+    s.completed && Boolean(s.response);
+
+export type UncompletedStep = Omit<Step, "completed"> & {
+    completed: false;
+};
+export const isStepUncompleted = (s: Step): s is CompletedStep => !s.completed && !s.response;
+
+export type SelectStep = Omit<Step, "options"> & {
+    options: NonNullable<Step["options"]>;
+};
+
+export type SelectOption = {
+    label: string;
+    value: string;
+};
+
+export type StepType = "select" | "plain";
