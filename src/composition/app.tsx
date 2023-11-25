6332bd38306b15d923f07d439b2a8dbce24ccce6
Vladislav <ivanovlad2010@gmail.com>
2023-11-25 16:24:39 +0100

    main page boilerplate crated

--- src/composition/app.tsx
+++ src/composition/app.tsx
@@ -1,3 +1,13 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { Container } from "@chakra-ui/react";
+import { MainPage } from "../pages/main";
+
 export function App() {
-    return <div>hello</div>;
+    return (
+        <ChakraProvider>
+            <Container py={4} display="flex" flexDirection="column" alignItems="center">
+                <MainPage />
+            </Container>
+        </ChakraProvider>
+    );
 }
---
+++ src/pages/main.tsx
@@ -0,0 +1,9 @@
+import { Heading, Container } from "@chakra-ui/react";
+
+export const MainPage = () => {
+    return (
+        <Container w="100%">
+            <Heading>Приёмная</Heading>
+        </Container>
+    );
+};
