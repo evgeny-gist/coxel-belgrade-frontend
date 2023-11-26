2a436367e86fe1cf811da9a460dcea593665c6f0
Vladislav <ivanovlad2010@gmail.com>
2023-11-26 01:13:01 +0100

    request form layout created

--- src/pages/main/components/requests-form.tsx
+++ src/pages/main/components/requests-form.tsx
@@ -1,12 +1,78 @@
 import { requestsRepository } from "@repositories/requests";
 import { observer } from "mobx-react";
+import { Field, Form, Formik } from "formik";
+import { Request } from "@domain/request";
+import { FormControl, FormLabel, Input, Button, Textarea, Box, Text } from "@chakra-ui/react";
 
 export const RequestsForm = observer(() => {
-    const { enabled } = requestsRepository;
+    const { enabled, loading } = requestsRepository;
 
     if (!enabled) {
         return <></>;
     }
 
-    return <div>RequestsForm</div>;
+    const initialValues: Request = {
+        name: "",
+        email: "",
+        text: "",
+        files: [],
+        topic: "",
+        additional_contacts: [],
+    };
+
+    const handleSubmit = (value: Request): void => {
+        if (loading) {
+            return;
+        }
+
+        requestsRepository.submit(value);
+    };
+
+    return (
+        <Box p={8} borderRadius={16} borderColor="gray.300" borderWidth="1px">
+            <Text mb={6}>Опишите подробнее вашу проблему и мы свяжемся с вами</Text>
+            <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+                {() => (
+                    <Form>
+                        <Field name="name">
+                            {({ field }) => (
+                                // TODO solve typing issue
+                                <FormControl mb={3}>
+                                    <FormLabel>Ваше имя</FormLabel>
+                                    <Input {...field} placeholder="Имя" />
+                                </FormControl>
+                            )}
+                        </Field>
+                        <Field name="topic">
+                            {({ field }) => (
+                                <FormControl mb={3}>
+                                    <FormLabel>Тема обращения</FormLabel>
+                                    <Input {...field} placeholder="Тема" />
+                                </FormControl>
+                            )}
+                        </Field>
+                        <Field name="email">
+                            {({ field }) => (
+                                <FormControl mb={3}>
+                                    <FormLabel>Электронная почта</FormLabel>
+                                    <Input type="email" {...field} placeholder="Почта" />
+                                </FormControl>
+                            )}
+                        </Field>
+                        <Field name="text">
+                            {({ field }) => (
+                                <FormControl mb={5}>
+                                    <FormLabel>Текст обращения</FormLabel>
+                                    <Textarea {...field} placeholder="Текст обращения" />
+                                </FormControl>
+                            )}
+                        </Field>
+                        <Button colorScheme="blue" isLoading={loading} type="submit">
+                            Отправить
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
+        </Box>
+    );
 });
--- src/repositories/requests/requests-repository.ts
+++ src/repositories/requests/requests-repository.ts
@@ -1,3 +1,4 @@
+import { Request } from "@domain/request";
 import { makeAutoObservable } from "mobx";
 
 export class RequestsRepositories {
@@ -6,16 +7,29 @@ export class RequestsRepositories {
     }
 
     private _enabled = false;
+    private _loading = false;
 
     public get enabled(): boolean {
         return this._enabled;
+    }
+
+    public get loading(): boolean {
+        return this._loading;
     }
 
     public toggle(value: boolean): void {
         this.setEnabled(value);
     }
 
+    public submit(value: Request): void {
+        this.setLoading(true);
+    }
+
     private setEnabled(value: boolean): void {
         this._enabled = value;
+    }
+
+    private setLoading(value = true): void {
+        this._loading = value;
     }
 }
